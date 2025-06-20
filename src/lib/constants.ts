d26864ea0643fed046e30cceee1ec92913811710
Joerakesh <rakeshjoe52@gmail.com>
2025-06-20 15:17:14 +0530

    added razorpay

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,8 +3,8 @@
 // 1. Replace RAZORPAY_KEY_ID with your actual Razorpay Key ID
 // 2. Replace RAZORPAY_SECRET with your actual Razorpay Secret Key
 export const RAZORPAY_CONFIG = {
-  KEY_ID: "rzp_test_9WaALlk42rzp6s", // Replace with your Razorpay Key ID
-  SECRET: "W3bej06991TABjFB8CjD8JSX", // Replace with your Razorpay Secret Key
+  KEY_ID: "rzp_live_cQLVIocotBfACG", // Replace with your Razorpay Key ID
+  SECRET: "3HihQChGXqezBDhIXkSMd9l8", // Replace with your Razorpay Secret Key
 };
 
 export const COMPANY_INFO = {
